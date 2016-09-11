247a8831d41f44cad7b063bfbeade282ad0a1582
Graeme Bart <gbart@alumni.uwo.ca>
2016-09-10 22:29:05 -0400

    -other changes necessitate passing pointers to Neuron objects

--- NN_synapse.hpp
+++ NN_synapse.hpp
@@ -1,23 +1,24 @@
-class Synapse {
+class Synapse
+{
     private:
     double w;
 
-    Neuron SourceNeuron;
-    Neuron TargetNeuron;
+    Neuron *SourceNeuron;
+    Neuron *TargetNeuron;
 
     public:
     void transmit()
     {
-	TargetNeuron.forceAddNeuronTemp(w * SourceNeuron.getNeuronValue());
+        TargetNeuron->forceAddNeuronTemp(w * SourceNeuron->getNeuronValue());
     }
-
+    
     void setW(double new_W)
     {
-	w = new_W;
+        w = new_W;
     }
-
+    
     double getW()
     {
-	return w;
+        return w;
     }
 };
