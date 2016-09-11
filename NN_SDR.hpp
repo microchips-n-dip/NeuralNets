6e8bab5f8813c3c7ba3bac1a1e7009166f2037ff
Graeme Bart <gbart@alumni.uwo.ca>
2016-09-10 22:27:17 -0400

    -use std c math functions for simple things like absolute values of  floating point types -the rest of this file doesn't make sense

--- NN_SDR.hpp
+++ NN_SDR.hpp
@@ -1,8 +1,11 @@
+#include <cmath>
+// No idea wtf this is supposed to do... doesn't make much sense
+
 void configureSDR(NeuronUpdateRule& rule, double baseline, double decay)
 {
-    rule.update(Neuron neuron)
+    rule.update(Neuron neuron)  // huh??
     {
-	if(Abs(baseline-NeuronValue) < decay) NeuronValue = baseline; // Overshoot compensation
+        if(fabs(baseline-NeuronValue) < decay) NeuronValue = baseline; // Overshoot compensation
         else if(NeuronValue < baseline) NeuronValue += decay; // Less than case
         else if(NeuronValue > baseline) NeuronValue -= decay; // Greater than case
     }
