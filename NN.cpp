3d6cfd8f30c1ca2c26097977fddc181a2df1d93f
microchips-n-dip <xisavariable52@gmail.com>
2016-10-03 17:24:25 -0400

    Added include of netCUDA.cu
    NN.cpp - Included netCUDA.cu

--- NN.cpp
+++ NN.cpp
@@ -3,6 +3,11 @@
 #include "NN_synapse.hpp"
 #include "NN_network.hpp"
 
+// CUDA implementation inclusion
+#define CUDA_SUPPORT 0 // I did this conditional stuff a bit lazily
+#if CUDA_SUPPORT 1
+#include "netCUDA.cu"
+#endif
 
 
 int main(void)
