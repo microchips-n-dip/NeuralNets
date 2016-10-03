53cf6f9244a471717498c6edb9fadad8b9c53287
microchips-n-dip <xisavariable52@gmail.com>
2016-10-03 17:18:25 -0400

    Finished CUDA implementation
    netCUDA.cu - Finished :D

--- netCUDA.cu
+++ netCUDA.cu
@@ -1,5 +1,22 @@
 #if CUDA_SUPPORT 1
-__global__ void cudaNeuron(Network** network)
+__global__ void cudaNeurons(Network** network)
 {
-    int n = 
+    int n = threadIdx.x + (blockIdx.x * blockDimx.x);
+    (*(**network).getNeuron(n)).update();
 }
+	
+__global__ void cudaSynapses(Network** network)
+{
+    int s = threadIdx.x + (blockId.x * blockDimx.x);
+    (*(**network).getSynapse(s)).transmit();
+}
+
+Network::cudaPropagate()
+{
+    Network** dev_net;
+    int size = sizeof(Network*);
+    cudaMemcpy(dev_net, &this, size, cudaMemcpyHostToDevice);
+    cudaSynapses<<<getNeuronCount()/TPB_NET, TBP_NET>>>(dev_net);
+    cudaNeurons<<<getNeuronCount()/TPB_NET, TBP_NET>>>(dev_net);
+}
+#endif
