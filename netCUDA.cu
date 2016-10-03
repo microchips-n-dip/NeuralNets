2d953f11dc1e256113de8a64ca7ce78f250e63cb
microchips-n-dip <xisavariable52@gmail.com>
2016-10-03 17:27:20 -0400

    Fixing typos
    netCUDA.cu - Fixed some typos

--- netCUDA.cu
+++ netCUDA.cu
@@ -2,13 +2,13 @@
 #define TPB_NET 512
 __global__ void cudaNeurons(Network** network)
 {
-    int n = threadIdx.x + (blockIdx.x * blockDimx.x);
+    int n = threadIdx.x + (blockIdx.x * blockDim.x);
     (*(**network).getNeuron(n)).update();
 }
 	
 __global__ void cudaSynapses(Network** network)
 {
-    int s = threadIdx.x + (blockId.x * blockDimx.x);
+    int s = threadIdx.x + (blockIdx.x * blockDim.x);
     (*(**network).getSynapse(s)).transmit();
 }
 
@@ -17,7 +17,7 @@ Network::cudaPropagate()
     Network** dev_net;
     int size = sizeof(Network*);
     cudaMemcpy(dev_net, &this, size, cudaMemcpyHostToDevice);
-    cudaSynapses<<<getNeuronCount()/TPB_NET, TBP_NET>>>(dev_net);
-    cudaNeurons<<<getNeuronCount()/TPB_NET, TBP_NET>>>(dev_net);
+    cudaSynapses<<<getNeuronCount()/TPB_NET, TPB_NET>>>(dev_net);
+    cudaNeurons<<<getNeuronCount()/TPB_NET, TPB_NET>>>(dev_net);
 }
 #endif
