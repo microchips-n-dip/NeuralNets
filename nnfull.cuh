f8d0f3c0adca4c79a07ab5b3410d74dc47d2b4c6
microchips-n-dip <xisavariable52@gmail.com>
2016-10-06 13:10:33 -0400

    Finished addNeuron and addSynapse functions
    nnfull.cuh - Finished implementing addNeuron and addSynapse. Also worked on the CUDA propagate funtion

--- nnfull.cuh
+++ nnfull.cuh
@@ -42,9 +42,9 @@ class Synapse {
 class Network {
   public:
     Neuron** neur; // Pointer for the neuron array
-    Neuron*** dev_neur; // Device copy of neur
+    Neuron** dev_neur; // Device copy of neur
     Synapse** syn; // Pointer for the synapse array
-    Synapse*** dev_syn; // Device copy of syn
+    Synapse** dev_syn; // Device copy of syn
     
     unsigned int n_neur; // Number of neurons in the network
     unsigned int n_syn; // Number of synapses in the network
@@ -54,32 +54,57 @@ class Network {
     {
 	Neuron** temp_neur = (Neuron**) malloc(n_neur * sizeof(Neuron*)); // Create temp to store current neurons
 	for(int i = 0; i < n_neur; i++) temp_neur[i] = neur[i]; // Copy neurons to temp
-	free(neur);
+	free(neur); // Free up neurons, will allocate again later
 
-	neur = (Neuron**) malloc((n_neur + 1) * sizeof(Neuron*));
-	for(int i = 0; i < n_neur; i++) neur[i] = temp_neur[i];
-	neur[n_neur] = neuron;
+	neur = (Neuron**) malloc((n_neur + 1) * sizeof(Neuron*)); // Allocate the new neuron array
+	for(int i = 0; i < n_neur; i++) neur[i] = temp_neur[i]; // Copy the temp back to the neuron array
+	neur[n_neur] = neuron; // Add the new neuron to the neuron array
 
-	n_neur++;
+	n_neur++; // Increment the size
+    }
+	
+    void addSynapse(Synapse* synapse)
+    {
+	Synapse** temp_syn = (Synapse**) malloc(n_syn * sizeof(Synapse*)); // Create temp to store current synapses
+	for(int i = 0; i < n_syn; i++) temp_syn[i] = syn[i]; // Copy synapses to temp
+	free(syn); // Free up synapses, will allocate again later
+
+	syn = (Synapse**) malloc((n_syn + 1) * sizeof(Synapse*)); // Allocate the new synapse array
+	for(int i = 0; i < n_syn; i++) syn[i] = temp_syn[i]; // Copy the temp back to the synapse array
+	syn[n_syn] = synapse; // Add the new synapse to the synapse array
+
+	n_syn++; // Increment the size
     }
     
 #if CUDA_SUPPORT == 1
-    __global__ void cudaNeuron(Neuron ***neuron) // Use the GPU to update neurons
+	int n_gpu = 1; // Default number of GPUs
+	
+    __global__ void cudaNeuron(Neuron **neuron) // Use the GPU to update neurons
     {
         int n = threadIdx.x + (blockIdx.x * blockDim.x); // Thread index (neuron number)
-        (*((*neuron)[n])).update(); // Update the neuron at the thread index
+        (*(neuron[n])).update(); // Update the neuron at the thread index
     }
     
-    __global__ void cudaSynapse(Synapse ***synapse) // Use the GPU to update synapses
+    __global__ void cudaSynapse(Synapse **synapse) // Use the GPU to update synapses
     {
         int n = threadIdx.x + (blockIdx.x * blockDim.x); // Thread index (synapse number)
-        (*((*synapse)[n])).transmit(); // Update the synapse at the thread index
+        (*(synapse[n])).transmit(); // Update the synapse at the thread index
     }
     
-    void propagate()
+    void propagate() // Propagate data through the network using the GPU(s)
     {
-        cudaNeuron<<<>>>();
-        cudaSynapse<<<>>>();
+		// Note: Multiple streams not yet implemented
+		cudaStream_t* streams = (cudaStream_t*) malloc(n_gpu*sizeof(cudaStream_t)); // Array to store CUDA streams
+		cudaEvent_t* events = (cudaEvent_t*) malloc(n_gpu*sizeof(cudaStream_t)); // Array to store CUDA events
+		
+		cudaMalloc((void***)&dev_neur, n_neur*sizeof(Neuron*)); // Allocate the neuron array on the GPU(s)
+		cudaMalloc((void***)&dev_syn, n_syn*sizeof(Synapse*)); // Allocate the synapse array on the GPU(s)
+		
+		cudaMemcpy(dev_neur, neur, n_neur*sizeof(Neuron*), cudaMemcpyHostToDevice); // Copy the neuron array to the GPU(s)
+		cudaMemcpu(dev_syn, syn, n_syn*sizeof(Synapse*), cudaMemcpyHostToDevice); // Copy the synapse array to the GPU(s)
+		
+		cudaSynapse<<<n_syn, 1>>>(dev_syn); // Execute the synapse updates on the GPU(s)
+        cudaNeuron<<<n_neur, 1>>>(dev_neur); // Execute the neuron updates on the GPU(s)
     }
 #endif
 
