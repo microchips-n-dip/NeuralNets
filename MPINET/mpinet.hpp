627a114578b6bce9569b897b9d782d1b512837b9
microchips-n-dip <xisavariable52@gmail.com>
2016-10-11 10:55:07 -0400

    Fixed spacing

--- MPINET/mpinet.hpp
+++ MPINET/mpinet.hpp
@@ -11,121 +11,123 @@ int n_procs;
 
 class Neuron {
   public:
-	double nv = 0.0;
-	double nvt = 0.0;
+    double nv = 0.0;
+    double nvt = 0.0;
 
-	virtual void update() = 0;
+    virtual void update() = 0;
 };
 
 class NeuronDecay: public Neuron {
   public:
-	double decay = 0.0;
-	double baseline = 0.0;
+    double decay = 0.0;
+    double baseline = 0.0;
 
-	void update()
-	{
-		// Decay in the direction of the baseline
-		if(nv > baseline) nv -= decay;
-		else if(nv < baseline) nv += decay;
-		if(fabs(nv - baseline) < decay) nv = decay; // Overshoot compensation
+    void update()
+    {
+	// Decay in the direction of the baseline
+	if(nv > baseline) nv -= decay;
+	else if(nv < baseline) nv += decay;
+	if(fabs(nv - baseline) < decay) nv = decay; // Overshoot compensation
 
-		// Adding the input, then resetting
-		nv += nvt;
-		nvt = 0.0;
-	}
+	// Adding the input, then resetting
+	nv += nvt;
+	nvt = 0.0;
+    }
 };
 
 struct NeuronLocale {
-	int my_owner;
-	Neuron *neuron;
+  public:
+    int my_owner;
+    Neuron *neuron;
 };
 
 NeuronLocale *createNeuron(Neuron *neuron)
 {
-	NeuronLocale *neuronLocale;
+    NeuronLocale *neuronLocale;
 
-	int n_neurons;
-	MPI_Allreduce(&proc_neurons, &n_neurons, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-	int owner = n_neurons % n_procs;
-	proc_neurons++;
+    int n_neurons;
+    MPI_Allreduce(&proc_neurons, &n_neurons, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    int owner = n_neurons % n_procs;
+    proc_neurons++;
 
-	int w_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
+    int w_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
 
-	(*neuronLocale).my_owner = owner;
-	(*neuronLocale).neuron = neuron;
+    (*neuronLocale).my_owner = owner;
+    (*neuronLocale).neuron = neuron;
 
-	if (w_rank == owner) {
-		return neuronLocale;
-	} else {
-		return NULL;
-	}
+    if (w_rank == owner) {
+	return neuronLocale;
+    } else {
+	return NULL;
+    }
 }
 
 
 class Synapse {
   public:
-	double w = 0.0;
-	NeuronLocale *Source;
-	NeuronLocale *Target;
+    double w = 0.0;
+    NeuronLocale *Source;
+    NeuronLocale *Target;
 };
 
 struct SynapseLocale {
-	int my_owner;
-	Synapse *synapse;
+  public:
+    int my_owner;
+    Synapse *synapse;
 };
 
 SynapseLocale *createSynapse(Synapse *synapse)
 {
-	SynapseLocale *synapseLocale;
+    SynapseLocale *synapseLocale;
 
-	int n_synapses;
-	MPI_Allreduce(&proc_synapses, &n_synapses, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-	int owner = n_synapses % n_procs;
-	proc_synapses++;
+    int n_synapses;
+    MPI_Allreduce(&proc_synapses, &n_synapses, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    int owner = n_synapses % n_procs;
+    proc_synapses++;
 
-	int w_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
+    int w_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
 
-	(*synapseLocale).my_owner = owner;
-	(*synapseLocale).synapse = synapse;
+    (*synapseLocale).my_owner = owner;
+    (*synapseLocale).synapse = synapse;
 
-	if (w_rank == owner) {
-		return synapseLocale;
-	} else {
-		return NULL;
-	}
+    if (w_rank == owner) {
+	return synapseLocale;
+    } else {
+	return NULL;
+    }
 }
 
 
 class Network {
   public:
-	vector<Neuron*> proc_neur;
-	vector<Synapse*> proc_syn;
+    vector<Neuron*> proc_neur;
+    vector<Synapse*> proc_syn;
 
-	void addNeuron(NeuronLocale *neuronLocale) // Determining an owner and assigning the neuron
-	{
-		int owner = (*neuronLocale).my_owner;
+    void addNeuron(NeuronLocale *neuronLocale) // Determining an owner and assigning the neuron
+    {
+	int owner = (*neuronLocale).my_owner;
 
-		int w_rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
+	int w_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
 
-		if (w_rank == owner) {
-			Neuron *neuron = (*neuronLocale).neuron;
-			neur.push_back(neuron);
-		}
+	if (w_rank == owner) {
+	    Neuron *neuron = (*neuronLocale).neuron;
+	    neur.push_back(neuron);
 	}
+    }
 
-	void addSynapse(SynapseLocale *synapseLocale) // Determining an owner and assigning the synapse
-	{
-		int owner = (*synapseLocale).my_owner;
+    void addSynapse(SynapseLocale *synapseLocale) // Determining an owner and assigning the synapse
+    {
+	int owner = (*synapseLocale).my_owner;
 
-		int w_rank;
-		MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
+	int w_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &w_rank);
 
-		if (w_rank == owner) {
-			Synapse *synapse = (*synapseLocale).synapse;
-			syn.push_back(synapse);
-		}
+	if (w_rank == owner) {
+	    Synapse *synapse = (*synapseLocale).synapse;
+	    syn.push_back(synapse);
 	}
+    }
 };
