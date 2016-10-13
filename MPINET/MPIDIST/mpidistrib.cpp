41e88387a094a02fee0e733933a68f4f650cb935
microchips-n-dip <xisavariable52@gmail.com>
2016-10-13 20:37:38 +0000

    Finished tree-like distributor

--- MPINET/MPIDIST/mpidistrib.cpp
+++ MPINET/MPIDIST/mpidistrib.cpp
@@ -22,20 +22,21 @@ void tree(Parallel &mpi, int idata)
     
     int recv;
     int send;
-    MPI_Status *stat;
     
-    for (int layer = 0; layer <= log2(mpi.nprocs); layer++)
+    for (int layer = 0; layer < log2(mpi.nprocs); layer++)
     {
+//	printf("Starting layer %i\n", layer);
         if (mpi.rank < pow(2, layer))
         {
-            recv = mpi.rank + pow(2, layer-1);
+            recv = mpi.rank + pow(2, layer);
+	    if (recv < mpi.nprocs)
             MPI_Send(data, 1, MPI_INT, recv, 0, MPI_COMM_WORLD);
         }
         
         else if (mpi.rank >= pow(2, layer) && mpi.rank < pow(2, layer + 1))
         {
             send = mpi.rank - pow(2, layer);
-            MPI_Recv(data, 1, MPI_INT, send, 0, MPI_COMM_WORLD, stat);
+            MPI_Recv(data, 1, MPI_INT, send, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("Process %i just got the message\n", mpi.rank);
             printf("Received value of %i\n", *data);
         }
