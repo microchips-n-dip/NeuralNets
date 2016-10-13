ae397033174a4978a1ee0e2e6232bdc9cd85f0d4
microchips-n-dip <xisavariable52@gmail.com>
2016-10-13 12:40:20 -0400

    Made a tree-like distributor

--- MPINET/mpidistrib.cpp
+++ MPINET/mpidistrib.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <mpi.h>
 
 #include <vector>
@@ -7,30 +8,46 @@
 
 #include "parallel.hpp"
 
+double log2(double a)
+{
+    return log(a) / log(2);
+}
+
+void tree(Parallel &mpi, int idata)
+{
+    int *data;
+    if (mpi.rank == 0) data = &idata;
+    
+    for (int layer = 0; layer <= log2(nprocs); layer++)
+    {
+        if (mpi.rank < pow(2, layer))
+        {
+            recv = mpi.rank + pow(2, layer-1);
+            MPI_Send(data, 1, MPI_INT, recv, 0, MPI_COMM_WORLD);
+        }
+        
+        else if (mpi.rank >= pow(2, layer) && mpi.rank < pow(2, layer + 1))
+        {
+            send = mpi.rank - pow(2, layer);
+            MPI_Recv(data, 1, MPI_INT, send, 0, MPI_COMM_WORLD);
+            printf("Process %i just got the message\n", mpi.rank);
+            printf("Received value of %i\n", *data);
+        }
+    }
+}
+
 int main(void)
 {
     MPI_Init(NULL, NULL);
     Parallel mpi();
     
-    int local_count;
+    int number;
     
     if (mpi.rank == 0)
     {
-        local_count = 3;
+        number = 3;
     }
     
-    else if (mpi.rank == 1)
-    {
-        local_count = 2;
-    }
-    
-    else if (mpi.rank == 2)
-    {
-        local_count = 0;
-    }
-    
-    else if (mpi.rank == 3)
-    {
-        local_count = 3;
-    }
+    // Now for the tree
+    tree(mpi, number, 0)
 }
