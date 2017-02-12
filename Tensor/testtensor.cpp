59069127c21d94d198e49283954025d6b84acdbc
microchips-n-dip <xisavariable52@gmail.com>
2017-02-12 17:45:46 +0000

    Organizing a bit

---
+++ Tensor/testtensor.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <stdarg.h>
+
+#include "tensor.hpp"
+
+int main(void)
+{
+    // Create a new tensor
+    Tensor<double> A(3, 5, 5, 2);
+}
