8dd2f32dda7a3de94214760ef5d6fae396995c02
Graeme <graemebart@gmail.com>
2017-02-12 17:25:46 -0500

    -too much to explain -gutted old code (GORY!) -replaced basically everything: special members, allocation/deletion,  multidimensional (abstract) member access in memory, setting/accessing  values, error messages, and more -removed unnecessary includes, typedef, namespace declarations and  extra levels of abstraction (TensorBlock) -created makefile

---
+++ Tensor/tensor.cpp
@@ -0,0 +1,16 @@
+//
+
+#include "tensor.hpp"
+
+int main(void)
+{
+    // Create a new tensor
+    Tensor<double> A(3, 5, 5, 2);
+    Tensor<double> B(4, 5, 5, 2, 6);
+    
+    printf("A.data[%i,%i,%i] = %lf\n",1,2,0,A.at(1,2,0));
+    printf("B.data[%i,%i,%i,%i] = %lf\n",3,1,4,1,B.at<4>({3,4,1,1}));
+    
+    A.assign({1,2,1},9.9);
+    printf("A.data[%i,%i,%i] = %lf\n",1,2,1,A.at(1,2,1));
+}
--- Tensor/tensor.hpp
+++ Tensor/tensor.hpp
@@ -1,113 +1,160 @@
 // tensor.hpp
 
-// The issues occur in TensorBlock
+#include <cstdio>
+#include <iostream>
+#include <cstdarg>
 
-namespace __CXX_TENSOR_SUB
-{
-	struct TensorBlock
-	{
-	  private:
-		// These are the member variables causing segfault
-		void* block;
-		std::vector<int> config;
-		int myrank;
-	
-	  public:
-		/*
-		Allocate aims to create a space in memory for a new tensor
-		*/
-		void allocate(int _rank, int size, va_list* ap)
-		{
-			// This causes a segmentation fault
-			myrank = _rank;
-		
-			int u = 1;
-			for (int i = 0; i < _rank; i++)
-			{
-				// This causes a segmentation fault
-				config.push_back(*va_arg((*ap), int));
-				// This causes a segmentation fault
-				u *= config[i];
-			}
-		
-			printf("Allocating block of size %d\n", u * size);
-			// This causes a segmentation fault
-			block = malloc(u * size);
-		}
-		
-		// Just freeing up tensor's memory space
-		void destruct()
-		{
-			free(block);
-		}
-		
-		// Not done this, haven't noticed any problems here yet
-		void getIndex(void* rLoc, va_list* ap)
-		{
-			std::vector<int> index;
-			for (int i = 0; i < myrank - 1; i++)
-			{
-				index.push_back(va_arg(ap, int));
-			}
-		
-			int blockL = 0;
-			int C = 1;
-		
-			for (int i = 0; i < myrank; i++)
-			{
-				blockL += index[i] * C;
-				C *= config[i];
-			}
-		
-			printf("Computed location in block to be %d\n", blockL);
-		
-			rLoc = block + blockL;
-		}
-	};
-};
-
-template <typename _Type>
+template <typename Type>
 class Tensor
 {
   private:
-	typedef _Type Type;
-  
-	__CXX_TENSOR_SUB::TensorBlock* block;
-	
-	
+    
+    /********************** Private data members *********************/
+    Type *_data;
+    unsigned int *dims;
+    unsigned int *stride;
+    unsigned int rank;
+    size_t total_elements;
+    
+    /******************** Private member functions *******************/
+    // Error message for bad access
+    template <typename A>
+      void access_error(std::initializer_list<A> args)
+    {
+        typename std::initializer_list<A>::iterator it;
+        
+        printf("Error: Invalid access in Tensor:\n");
+        printf("\t[");
+        for (it=args.begin(); it<args.end()-1; ++it)
+            printf("%d ",*it);
+        printf("%d] is an invalid element in Tensor class object at %p\n",*(args.end()-1),this);
+        printf("\twith dimensions [%u ",dims[0]);
+        for (unsigned int r=1; r<rank-1; ++r)
+            printf("%u ",dims[r]);
+        printf("%u].\n",dims[rank-1]);
+        abort();
+    }
+    
+    // Error message for accessing out of bounds
+    void bounds_error(size_t ind)
+    {
+        printf("Error: Invalid access in Tensor::at():\n");
+        printf("\telement %lu is out of bounds in a list of size %lu\n",ind,total_elements);
+        abort();
+    }
+    
+    // Error message for provided number of dimensions exceeding rank
+    void dim_error(int n)
+    {
+        printf("Number of dimensions exceeds specified tensor rank.\n");
+        abort();
+    }
+    
+    // Helper function to calculate index of requested element in _data
+    template <typename A>
+      inline unsigned int get_index(std::initializer_list<A> args)
+    {
+        unsigned int *str = stride;     // Local pointer to stride list
+        unsigned int *dm = dims;        // Local pointer to dimensions list
+        unsigned int ind = 0;           // Index of data to access
+        
+        // Loop through the array-initialized list and calculate the index
+        typename std::initializer_list<A>::iterator li;
+        for (li=args.begin(); li<args.end(); ++li, ++str, ++dm)
+        {
+            if (*li >= *dm) access_error(args); // Error if bad element
+            ind += *li*(*str);
+        }
+        
+        if (ind >= total_elements) bounds_error(ind);   // Error if out of bounds
+        return ind;
+    }
+    
+    // Error message for bad rank
+    void rank_error(int n)
+    {
+        printf("Specified tensor rank exceeds number of dimensions provided.\n");
+        abort();
+    }
+    
+    
   public:
-	Tensor(int _rank, ...)
-	{
-		printf("Allocating tensor space...\n");
-		
-		va_list ap;
-		va_start(ap, &_rank);
-		
-		(*block).allocate(_rank, sizeof(Type), &ap);
-		
-		va_end(ap);
-		
-		printf("Successfully allocated tensor space!\n");
-	}
-	
-	~Tensor()
-	{
-		printf("Freeing tensor space...\n");
-		(*block).destruct();
-		printf("Successfully freed tensor space!\n");
-	}
-	
-	Type at(int index1, ...)
-	{
-		void* R;
-		
-		va_list ap;
-		va_start(ap, (&index1) - 1);
-		
-		(*block).getIndex(R, &ap);
-		
-		va_end(ap);
-		
-		return *((Type*)(R));
-	}
+    
+    /************************ Special members ************************/
+    // Constructor
+    Tensor(int _rank, ...): rank(_rank)
+    {
+        va_list ap;
+        va_start(ap, _rank);
+        
+        unsigned int n = 0;     // Counter to check dims
+        
+        // Allocations
+        dims = new unsigned int[_rank];
+        stride = new unsigned int[_rank];
+        total_elements = 1;
+        for (int r=0; r<_rank; ++r, ++n)
+        {
+            int val = va_arg(ap,int);
+            stride[r] = total_elements;
+            total_elements *= val;
+            dims[r] = val;
+            
+            if (n>=_rank) dim_error(n); // If looping past rank of tensor
+        }
+        if (n < _rank) rank_error(n);   // If insufficient rank
+        va_end(ap);
+        
+        // Allocate the data buffer
+        _data = new Type[total_elements];
+    }
+    
+    // Copy constructor
+    // seems std c++ does not allow templated copy constructors?
+    
+    // Destructor
+    ~Tensor()
+    {
+        delete[] _data;
+        delete[] dims;
+        delete[] stride;
+    }
+
+    /******************** Public member functions ********************/
+    // Rank 1 overload for efficiency
+    Type at(int i)
+    {
+        return _data[i];
+    }
+
+    // Rank 2 overload for efficiency
+    Type at(int i, int j)
+    {
+        unsigned int ind = i + stride[1]*j;
+        return _data[ind];
+    }
+    
+    // Rank 3 overload for efficiency
+    Type at(int i, int j, int k)
+    {
+        if (i<dims[0] && (j<dims[1] && k<dims[2]))
+        {
+            unsigned int ind = i + stride[1]*j + stride[2]*k;
+            return _data[ind];
+        }
+    }
+
+    // General overload for arbitrary rank
+    template <unsigned int R, typename A>
+      Type& at(std::initializer_list<A> args)
+    {
+        return _data[get_index(args)];
+    }
+    
+    // Set a specific element to some value
+    template <typename A>
+      void assign(std::initializer_list<A> args, Type val)
+    {
+        _data[get_index(args)] = val;
+    }
 };
