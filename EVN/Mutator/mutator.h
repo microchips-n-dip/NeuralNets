a86d588a1099b299290335867ea5de514563a470
microchips-n-dip <xisavariable52@gmail.com>
2017-08-08 00:15:03 +0000

    Fixed indentation

--- EVN/Mutator/mutator.h
+++ EVN/Mutator/mutator.h
@@ -19,35 +19,35 @@ struct Mutator
   std::uniform_real_distribution<Derived> ud;
 	std::default_random_engine generator;
   std::random_device rdevice;
-  
+
   // Distribution's standard deviation (default to 0.2)
   constexpr static double s = 0.2;
-  
-	void reset_nd(double m)
-	{ nd = std::normal_distribution<Derived>(m, s); }
-	
+
+  void reset_nd(double m)
+  { nd = std::normal_distribution<Derived>(m, s); }
+
   void reset_nd(double m, double _s)
   { nd = std::normal_distribution<Derived>(m, _s); }
-  
+
   Derived get_nd()
   { return nd(generator); }
-  
+
   void reset_ud(Derived a, Derived b)
   { ud = std::uniform_real_distribution<Derived>(a, b); }
-  
+
   Derived get_ud()
   { return ud(generator); }
-  
+
   Mutator() {
-		generator = std::default_random_engine(rdevice());
-	}
-	
-	void operator=(const Mutator& mu)
-	{
-		nd = mu.nd;
-		ud = mu.ud;
-		generator = mu.generator;
-	}
+    generator = std::default_random_engine(rdevice());
+  }
+
+  void operator=(const Mutator& mu)
+  {
+    nd = mu.nd;
+    ud = mu.ud;
+    generator = mu.generator;
+  }
 };
 
 #endif
