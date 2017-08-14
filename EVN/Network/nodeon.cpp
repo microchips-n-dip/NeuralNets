b300f6d156980ecafd9d04d07b37d1163b1b3bbf
microchips-n-dip <xisavariable52@gmail.com>
2017-08-14 03:08:22 +0000

    Commented throughout nodeon.cpp

--- EVN/Network/nodeon.cpp
+++ EVN/Network/nodeon.cpp
@@ -3,8 +3,10 @@
 const double pi = acos(-1);
 const double npi = std::sqrt(0.25 * pi);
 
+// Nodeon constructor
 Nodeon::Nodeon(Network* _network)
 {
+  // Set the internal pointer to the network
   network = _network;
 
   // Default type of nodeon
@@ -15,6 +17,7 @@ Nodeon::Nodeon(Network* _network)
   m_llft = 0;
 }
 
+// Remove all attached connectons
 void Nodeon::self_destruct()
 {
   while (src_connectons.size() > 0)
@@ -23,6 +26,7 @@ void Nodeon::self_destruct()
     (*dst_connectons.begin())->self_destruct();
 }
 
+// Make a new connecton connected to a random node
 void Nodeon::add_connecton_with_random_nodes()
 {
   // Get sizes
@@ -33,18 +37,24 @@ void Nodeon::add_connecton_with_random_nodes()
     network->mu.reset_ud(0, nsz);
     Nodeon* dst_node = network->nodeons.begin() + mu.get_ud();
 
+    // Check for duplicates, we don't want those
     for (unsigned int i = 0; i < src_connectons.size(); ++i) {
+      // Exit the loop if all nodeons have been examined
+      if (dst_node >= network->nodeons.end())
+        break;
       if (src_connectons[i]->dst == dst_node) {
         ++dst_node;
         i = 0;
       }
     }
 
+    // If no duplicates are found, create the new connecton
     if (dst_node < network->nodeons.end())
       src_connectons.push_back(new Connecton(this, dst_node, network, false));
   }
 }
 
+// Remove a random connecton
 void Nodeon::remove_random_connecton()
 {
   unsigned int csz = src_connectons.size();
@@ -56,8 +66,11 @@ void Nodeon::remove_random_connecton()
   }
 }
 
+// Mutate the node,
+// add or remove connectons given by connecton_drift
 void Nodeon::node_mutate()
 {
+  // Mutate the nodeon type
   // Reset for type change
   network->mu.reset_nd(ntype, 0.2);
   ntype += network->mu.get_nd();
@@ -65,6 +78,7 @@ void Nodeon::node_mutate()
   if (ntype > 2) ntype = 2;
   if (ntype < -2) ntype = -2;
 
+  // Mutate the number
   if (ntype != 0) {
     network->mu.reset_nd(number, 0.4);
     number += network->mu.get_nd();
@@ -80,6 +94,7 @@ void Nodeon::node_mutate()
   if (connecton_drift < -5)
     connecton_drift = -5;
 
+  // Make sure there is no underflow error
   connecton_drift = (int(csz) > -connecton_drift) ? connecton_drift : 1 - csz;
 
   // Add/remove connectons until satisfied
@@ -91,41 +106,52 @@ void Nodeon::node_mutate()
   }
 }
 
+// Nodeon activation function
 void Nodeon::lf(double t)
 {
+  // Compute the activation as the sigmoid of the input
   m_activation = erf(npi * m_inp);
 
+  // Reset some internal variables
   m_spike_happened = false;
   m_inp = 0;
 
+  // Do type dependent activation jobs
   if (m_activation > 0.5) {
+    // Get an input
     if (ntype == INP_NODE) {
       if (number == network->c_input())
         m_activation = 1.0;
       else
         m_activation = 0.0;
     }
+    // Output a number
     else if (ntype == OUT_NODE) {
       network->c_output(number);
     }
+    // Increment global_i
     else if (ntype == INC_NODE) {
       ++(network->global_i);
     }
+    // Decrement global_i
     else if (ntype == DEC_NODE) {
       --(network->global_i);
     }
   }
 
+  // Log last firing time and set spike boolean
   if (m_activation > 0.5) {
     m_llft = t;
     m_spike_happened = true;
   }
 
+  // Transmit the activation across all connectons with this nodeon as a source
   unsigned int csz = src_connectons.size();
   for (unsigned int i = 0; i < csz; ++i) {
     src_connectons.at(i)->transmit();
   }
 
+  // Do STDP modulation on all connectons with this nodeon as a destination
   csz = dst_connectons.size();
   for (unsigned int i = 0; i < csz; ++i) {
     dst_connectons.at(i)->stdp();
