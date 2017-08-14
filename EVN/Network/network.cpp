f272c5f0d05fb4ac8636faf5e980e62b0b5ed00f
microchips-n-dip <xisavariable52@gmail.com>
2017-08-14 02:56:57 +0000

    Commented throughout network.cpp

--- EVN/Network/network.cpp
+++ EVN/Network/network.cpp
@@ -4,17 +4,21 @@ const unsigned int n_samples = 1;
 const unsigned int NetNumAllowedCycles = 1024;
 const unsigned int EvNumAllowedCycles = 256;
 
+// Get the current input at the location indicated by global_i
 unsigned int Network::c_input()
 {
   return m_input.at(global_i);
 }
 
+// Write to the current output
 void Network::c_output(unsigned int number)
 {
   m_output.at(global_j) = number;
+  // It is assumed that a network is never allowed to change the output it has written
   ++global_j;
 }
 
+// Remove a nodeon
 void Network::remove_random_nodeon()
 {
   unsigned int nsz = nodeons.size();
@@ -22,13 +26,18 @@ void Network::remove_random_nodeon()
   nodeons.at(mu.get_ud())->self_destruct();
 }
 
+// Add or remove some number of nodeons given by nodeon_drift,
+// then mutate each nodeon
 void Network::mutate()
 {
   // Sizes
+  // (it's much more convenient to type three letters)
   unsigned int nsz = nodeons.size();
   unsigned int csz = connectons.size();
 
   // Normal limits
+  // A way of making sure a network doesn't randomly occupy all the available memory
+  // after the first generation
   const unsigned int nlim = 2;
 
   // Reset for nodeon drift
@@ -40,6 +49,7 @@ void Network::mutate()
   if (nodeon_drift < -nlim)
     nodeon_drift = -nlim;
 
+  // Double check that there is no underflow problem, otherwise the program will take more than 224 GB
   nodeon_drift = (int(nsz) > -nodeon_drift) ? nodeon_drift : 1 - nsz;
 
   // Add/remove nodeons until satisfied
@@ -50,14 +60,17 @@ void Network::mutate()
       remove_random_nodeon(); // Handles any connectons attached
   }
 
+  // Mutate each node
   for (unsigned int i = 0; i < nodeons.size(); ++i) {
     nodeons.at(i)->node_mutate();
   }
 
+  // Invalidate the saved values
   fit_saved_valid = false;
   cost_saved_valid = false;
 }
 
+// Simulate each nodeon
 void Network::net_run(double time)
 {
   double stime = network_time;
@@ -68,38 +81,52 @@ void Network::net_run(double time)
   }
 }
 
+// Compute the cost value the network
 double Network::net_cost()
 {
+  // Check for valid saved value, if so return that (massive time saver)
   if (cost_saved_valid)
     return cost_saved;
+  // If not compute cost from scratch
   else {
     double misaligned = 0;
+    // Compute cost for each sample
     for (unsigned int i = 0; i < n_samples; ++i) {
       std::vector<unsigned int> output;
+      // Load the sample
       unsigned int sz_sample = load_sample(&m_input, &output, &m_output, i);
+      // Run the network for some cycles
       net_run(NetNumAllowedCycles);
 
+      // Calculate the misalignment of the outputs
       for (unsigned int j = 0; j < sz_sample; ++j) {
         misaligned += output[j] - m_output[j];
       }
     }
 
-    cost_saved = misaligned;
+    // Save and validate the cost
+    cost_saved = std::fabs(misaligned);
     cost_saved_valid = true;
 
     return std::fabs(misaligned);
   }
 }
 
+// Calculate the fitness of the network
+// Fitness is measured as the networks ability to improve
 double Network::net_fitness()
 {
+  // If there is a valid saved value, return that
   if (fit_saved_valid)
     return fit_saved;
   else {
+    // Compute the cost
     double c_cost = net_cost();
+    // Compare the current cost to the last one
     double ret = 1 - exp(-m_net_last_cost / c_cost);
     m_net_last_cost = c_cost;
 
+    // Save and validate the fitness
     fit_saved = ret;
     fit_saved_valid = true;
 
@@ -107,6 +134,7 @@ double Network::net_fitness()
   }
 }
 
+// Run the network with STDP for some cycles
 void Network::run(unsigned int cycles)
 {
   std::vector<unsigned int> o = m_output;
@@ -119,6 +147,7 @@ void Network::run(unsigned int cycles)
 
 Network::Network()
 {
+  // Initialize mu
   mu = Mutator<double>();
 
   global_i = 0;
@@ -126,6 +155,7 @@ Network::Network()
 
   network_time = 0;
 
+  // Set the last costs to infinity, to avoid false results
   m_net_last_cost = std::numeric_limits<double>::infinity();
   m_ev_last_cost = std::numeric_limits<double>::infinity();
 
--- EVN/evn.cpp
+++ EVN/evn.cpp
@@ -5,9 +5,6 @@
 
 int main(void)
 {
-  printf("Size of Nodeon: %d\n", sizeof(Nodeon));
-  printf("Size of Connecton: %d\n", sizeof(Connecton));
-
   static const unsigned int NumNets = 100;
   std::vector<Network*> networks;
   for (unsigned int i = 0; i < NumNets; ++i) {
@@ -21,23 +18,27 @@ int main(void)
     std::cin >> n_generations;
 
     for (unsigned int i = 0; i < n_generations; ++i) {
-      for (unsigned int j = 0; j < NumNets / 2; ++j)
-        networks.at(j)->run(1);
-      sort(networks);
-      for (unsigned int j = 0; j < NumNets / 2; ++j)
-        networks.at(j) = networks.at(j + NumNets / 2);
+      // Mutate this generation
       for (unsigned int j = 0; j < NumNets; ++j)
         networks.at(j)->mutate();
 
+      // Run each network
+      for (unsigned int j = 0; j < NumNets; ++j)
+        networks.at(j)->run(1);
+
+      // Sort and reproduce the networks
+      sort(networks);
+      for (unsigned int j = 0; j < NumNets / 2; ++j)
+        networks.at(j) = networks.at(j + NumNets / 2);
+
+      // Compute the average number of connectons per network
       double avg_connectons = 0;
       for (unsigned int i = 0; i < NumNets; ++i)
         avg_connectons += networks.at(i)->connectons.size();
       printf("Average of %f connectons per network\n", avg_connectons / NumNets);
     }
 
-    for (unsigned int j = 0; j < NumNets / 2; ++j)
-      networks.at(j)->run(1);
-    sort(networks);
+    // Print the score and configuration of the best network
     printf("Best fitness: %.32f\n", networks.at(NumNets - 1)->net_fitness());
     printf("Best cost: %.32f\n", networks.at(NumNets - 1)->net_cost());
     printf("Best network has %d nodeons and %d connectons\n", networks.at(NumNets - 1)->nodeons.size(), networks.at(NumNets - 1)->connectons.size());
