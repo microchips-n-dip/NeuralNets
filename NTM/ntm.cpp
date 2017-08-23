b3994569c1fdc23e8ebbc37582cf0896930c6d69
microchips-n-dip <xisavariable52@gmail.com>
2017-08-23 21:19:03 +0000

    Finished forward method

--- NTM/ntm.cpp
+++ NTM/ntm.cpp
@@ -1,4 +1,5 @@
 #include "ntm.h"
+#include "tensorops.h"
 
 using namespace Eigen;
 
@@ -6,9 +7,11 @@ Tensor<double, 1> forward(Tensor<double, 1> x)
 {
   Tensor<double, 1> prev_C;
   std::list<Tensor<double, 1>> prev_Cs = Cs;
-
-  // Clear state
   Cs = std::list<Tensor<double, 1>>(layers);
+
+  Tensor<double, 1> prev_o;
+  std::list<Tensor<double, 1>> prev_output_list = output_list;
+  output_list = std::list<Tensor<double, 1>>(layers);
 
   Tensor<double, 1> concat_input;
   concat_input = concat_input.concatenate(x);
@@ -30,7 +33,7 @@ Tensor<double, 1> forward(Tensor<double, 1> x)
     Tensor<double, 1> i_gate = sigmoid(tco(h, w_i_gate, ctr1));
     std::array<IndexPair<int>, 1> ctr2 = {IndexPair<int>(0, 0)};
     Tensor<double, 1> f_gate = sigmoid(tco(h, w_g_gate, ctr2));
-    std::array<IndexPair<int>, 1> ctr3 = {IndexPaur<int>(0, 0)};
+    std::array<IndexPair<int>, 1> ctr3 = {IndexPair<int>(0, 0)};
     Tensor<double, 1> o_gate = sigmoid(tco(h, w_o_gate, ctr3));
     std::array<IndexPair<int>, 1> ctr4 = {IndexPair<int>(0, 0)};
     Tensor<double, 1> u_gate = tanh(tco(h, w_u_gate, ctr4));
