2026c61f00b89951c35da55109305276062b899e
microchips-n-dip <xisavariable52@gmail.com>
2017-08-25 00:00:27 +0000

    Finished gradient calculation

--- NTM/ntm.cpp
+++ NTM/ntm.cpp
@@ -6,11 +6,11 @@ using namespace Eigen;
 Tensor<double, 1> forward(Tensor<double, 1> x)
 {
   Tensor<double, 1> prev_C;
-  std::list<Tensor<double, 1>> prev_Cs = Cs;
+  prev_Cs = Cs;
   Cs = std::list<Tensor<double, 1>>(layers);
 
   Tensor<double, 1> prev_o;
-  std::list<Tensor<double, 1>> prev_output_list = output_list;
+  prev_output_list = output_list;
   output_list = std::list<Tensor<double, 1>>(layers);
 
   Tensor<double, 1> concat_input;
@@ -35,34 +35,56 @@ Tensor<double, 1> forward(Tensor<double, 1> x)
     Tensor<double, 1> o_gate = sigmoid(tco(h, w_o_gate, ctr1));
     Tensor<double, 1> u_gate = tanh(tco(h, w_u_gate, ctr1));
 
+    ntm_i_gate.at(layer_idx) = i_gate;
+    ntm_f_gate.at(layer_idx) = f_gate;
+    ntm_o_gate.at(layer_idx) = o_gate;
+    ntm_u_gate.at(layer_idx) = u_gate;
+
     Tensor<double, 1> C;
     prev_C = prev_Cs.at(layer_idx);
     C = (prev_C * f_gate) + (i_gate * u_gate);
-    Cs.at(layer_idx) = C;
+    Cs.at(layer_idx) = C * o_gate;
+
+    zu.at(layer_idx) = C;
+    zo.at(layer_idx) = o_gate;
 
     h = tanh(C);
     output_list.at(layer_idx) = h;
   }
 }
 
-void backprop()
+void NTM::backprop()
 {
+  Tensor<double, 1> ph;
+
   for (unsigned int layer_idx = layers - 1; layer_idx > 0; layer_idx--) {
     Tensor<double, 2> w_i_gate = ntm_ig_weights.at(layer_idx);
     Tensor<double, 2> w_f_gate = ntm_fg_weights.at(layer_idx);
     Tensor<double, 2> w_u_gate = ntm_ug_weights.at(layer_idx);
     Tensor<double, 2> w_o_gate = ntm_og_weights.at(layer_idx);
 
-    tCu = tanh_prime(Cu);
+    Tensor<double, 1> i_gate = ntm_i_gate.at(layer_idx);
+    Tensor<double, 1> f_gate = ntm_f_gate.at(layer_idx);
+    Tensor<double, 1> o_gate = ntm_o_gate.at(layer_idx);
+    Tensor<double, 1> u_gate = ntm_u_gate.at(layer_idx);
+
+    Tensor<double, 1> Cu = Cs.at(layer_idx);
+    Tensor<double, 1> tCu = tanh_prime(Cu);
+
+    Tensor<double, 1> prev_C = prev_Cs.at(layer_idx);
 
     std::array<IndexPair<int>, 1> ctr1 = {IndexPair<int>(0, 0)};
-    Tensor<double, 1> delta_o = sigmoid_prime(tco(lambda, w_o_gate, ctr1));
+
+    double zeta_u = tco(ph, zu.at(layer_idx), ctr1);
+    double zeta_o = tco(ph, zo.at(layer_idx), ctr1);
+
+    Tensor<double, 1> delta_o = zeta_u * sigmoid_prime(tco(lambda, w_o_gate, ctr1));
     Tensor<double, 1> dudq = tanh_prime(tco(lambda, w_u_gate, ctr1));
-    Tensor<double, 1> delta_u = tco(tCu, i_gate, ctr1) * dudq;
+    Tensor<double, 1> delta_u = zeta_o * tco(tCu, i_gate, ctr1) * dudq;
     Tensor<double, 1> didq = sigmoid_prime(tco(lambda, w_i_gate, ctr1));
-    Tensor<double, 1> delta_i = tco(tCu, u_gate, ctr1) * didq;
+    Tensor<double, 1> delta_i = zera_o * tco(tCu, u_gate, ctr1) * didq;
     Tensor<double, 1> dfdq = sigmoid_prime(tco(lambda, w_f_gate, ctr1));
-    Tensor<double, 1> delta_f = tco(tCu, prev_C, ctr1) * dfdq;
+    Tensor<double, 1> delta_f = zeta_o * tco(tCu, prev_C, ctr1) * dfdq;
 
     std::array<Index<int>, 0> ctr2 = {};
     Tensor<double, 2> dhdWo = tco(delta_o, lambda, ctr2);
@@ -76,5 +98,7 @@ void backprop()
     delta_l += tco(delta_u, w_u_gate, ctr3);
     delta_l += tco(delta_i, w_i_gate, ctr3);
     delta_l += tco(delta_f, w_f_gate, ctr3);
+
+    ph = delta_l;
   }
 }
--- NTM/ntm.h
+++ NTM/ntm.h
@@ -1,28 +1,33 @@
 #ifndef NTM_H
 #define NTM_H
 
-template <int MemSize, MemLength>
+using Eigen::Tensor;
+
 class NTM
 {
   public:
 
-  // NTM weight matrices
-  std::list<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>> \
-    ntm_weights;
+  std::list<Tensor<double, 1>> Cs;
+  std::list<Tensor<double, 1>> prev_Cs;
 
-  // NTM memory
-  Eigen::Matrix<double, MemSize, MemLength> Memory;
+  std::list<Tensor<double, 2>> ntm_fg_weights;
+  std::list<Tensor<double, 2>> ntm_ig_weights;
+  std::list<Tensor<double, 2>> ntm_ug_weights;
+  std::list<Tensor<double, 2>> ntm_og_weights;
 
-  double w_beta;
-  double w_g;
-  double w_s; // S implementation remains to be seen
-  double w_gamma;
+  std::list<Tensor<double, 1>> ntm_f_gate;
+  std::list<Tensor<double, 1>> ntm_i_gate;
+  std::list<Tensor<double, 1>> ntm_u_gate;
+  std::list<Tensor<double, 1>> ntm_o_gate;
 
-  NTM();
-  ~NTM();
+  std::list<Tensor<double, 1>> zu;
+  std::list<Tensor<double, 1>> zo;
 
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>
-  forward(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> x);
+  std::list<Tensor<double, 1>> output_list;
+  std::list<Tensor<double, 1>> prev_output_list;
+
+  Tensor<double, 1> forward(Tensor<double, 1> x);
+  void backpropagate();
 };
 
 #endif
