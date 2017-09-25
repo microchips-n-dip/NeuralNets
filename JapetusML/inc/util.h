8d7c4038e81f5257fe8c02af1d4deff83cb6ebb7
microchips-n-dip <xisavariable52@gmail.com>
2017-09-25 14:08:59 +0000

    Added remove_const

--- JapetusML/inc/util.h
+++ JapetusML/inc/util.h
@@ -26,6 +26,9 @@ template<typename T> struct remove_all<T&>        { typedef typename remove_all<
 template<typename T> struct remove_all<T const*>  { typedef typename remove_all<T>::type type; };
 template<typename T> struct remove_all<T*>        { typedef typename remove_all<T>::type type; };
 
+template <typename T> struct remove_const { typedef T type; };
+template <typename T> struct remove_const<const T> { typedef typename remove_const<T>::type type; };
+
 template <bool condition, typename Then, typename Else>
 struct conditional { };
 
