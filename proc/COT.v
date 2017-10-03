3a4d5af471b7a0cacb027a5cc5cadb6931a01713
microchips-n-dip <xisavariable52@gmail.com>
2017-10-02 23:32:13 -0400

    Finished neighbor address computation

--- proc/COT.v
+++ proc/COT.v
@@ -44,16 +44,24 @@ parameter x = i % n;
 parameter y = floor(i / n);
 
 // Core neighbor address computation
-parameter nei_1_ = n * y + ((x + 1) % n);
-parameter nei_2_ = n * y + ((x + 2) % n);
-parameter nei_1 = (nei_1_ < nei_2_) ? nei_1_ : nei_2_;
-parameter nei_2 = (nei_1_ < nei_2_) ? nei_2_ : nei_1_;
-parameter nei_3_ = n * ((y + 1) % n) + x;
-parameter nei_4_ = n * ((y + 2) % n) + x;
-parameter nei_3 = (nei_3_ < nei_4_) ? nei_3_ : nei_4_;
-parameter nei_4 = (nei_3_ < nei_4_) ? nei_4_ : nei_3_;
-parameter nei_5 = ;
-parameter nei_6 = ;
+parameter nei_0_x = (x + 1) % n;
+parameter nei_0_ = n * y + nei_0_x;
+parameter nei_1_x = (x + n - 1) % n;
+parameter nei_1_ = n * y + nei_1_x;
+// Switch in case of left wrapping
+parameter nei_0 = ((nei_1_x - x) == 1) ? nei_0_ : nei_1_;
+parameter nei_1 = ((nei_1_x - x) == 1) ? nei_1_ : nei_0_;
+parameter nei_2_y = (y + 1) % n;
+parameter nei_2_ = n * nei_2_y + x;
+parameter nei_3_y = (y + n - 1) % n;
+parameter nei_3_ = n * nei_3_y + x;
+// Switch in case of top wrapping
+parameter nei_2 = ((nei_3_y - y) == 1) ? nei_2_ : nei_3_;
+parameter nei_3 = ((nei_3_y - y) == 1) ? nei_3_ : nei_2_;
+parameter en_nei_4 = 0;
+parameter nei_4 = 0;
+parameter en_nei_5 = 0;
+parameter nei_5 = 0;
 
 reg dests [0:5];
 
@@ -85,14 +93,18 @@ wire c6 [0:3];
 wire c7 [0:3];
 wire c8 [0:3];
 
-Core #(.i(0), .n(3)) core0(clk);
-Core #(.i(1), .n(3)) core1(clk);
-Core #(.i(2), .n(3)) core2(clk);
-Core #(.i(3), .n(3)) core3(clk);
-Core #(.i(4), .n(3)) core4(clk);
-Core #(.i(5), .n(3)) core5(clk);
-Core #(.i(6), .n(3)) core6(clk);
-Core #(.i(7), .n(3)) core7(clk);
-Core #(.i(8), .n(3)) core8(clk); 
+//   2
+// 0   1
+//   3
+
+Core #(.i(0), .n(3)) core0(clk, {c1[0], c2[1], c3[2], c6[3]}, c0);
+Core #(.i(1), .n(3)) core1(clk, {c0[1], c2[0], c4[2], c7[3]}, c1);
+Core #(.i(2), .n(3)) core2(clk, {c0[0], c1[1], c5[2], c8[3]}, c2);
+Core #(.i(3), .n(3)) core3(clk, {c4[0], c5[1], c0[3], c6[2]}, c3);
+Core #(.i(4), .n(3)) core4(clk, {c3[1], c5[0], c1[3], c7[2]}, c4);
+Core #(.i(5), .n(3)) core5(clk, {c3[0], c4[1], c2[3], c8[2]}, c5);
+Core #(.i(6), .n(3)) core6(clk, {c7[0], c8[1], c0[3], c2[2]}, c6);
+Core #(.i(7), .n(3)) core7(clk, {c6[1], c8[0], c1[3], c4[2]}, c7);
+Core #(.i(8), .n(3)) core8(clk, {c6[0], c7[1], c2[3], c5[2]}, c8); 
 
 endmodule
